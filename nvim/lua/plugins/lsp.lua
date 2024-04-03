2027ca198f044d24d7699191ea27faa5c2a1d1e5
Philippe Lopes <philippe.lopes@live.fr>
2024-04-03 18:05:56 +0900

    Added LSP to nvim

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,104 @@
+return {
+    'williamboman/mason.nvim',
+    dependencies = {
+        -- Core
+        'williamboman/mason-lspconfig.nvim',
+        'neovim/nvim-lspconfig',
+        -- Completion
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        -- Lua Snippets
+        'L3MON4D3/LuaSnip',
+    },
+
+    config = function()
+        local servers_list = {
+            'ansiblels', -- Ansible
+            'bashls', -- Bash
+            'cssls', -- CSS
+            'dockerls', -- Docker
+            'docker_compose_language_service', -- Docker Compose
+            'html', -- HTML
+            'tsserver', -- JS/TS
+            'lua_ls', -- Lua
+            'marksman', -- Markdown
+            'pyright', -- Python
+            'rust_analyzer', -- Rust
+            'sqlls', -- SQL
+            'tailwindcss', -- TailwindCSS
+            'terraformls', -- Terraform
+            'yamlls', -- YAML
+        }
+        
+        require('mason').setup()
+        require('mason-lspconfig').setup({
+            ensure_installed = servers_list
+        })
+
+        -- Settings variables
+        local cmp = require('cmp')
+        local luasnip = require('luasnip')
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lsp_config = require('lspconfig')
+
+        -- Completion settings
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                    { name = 'buffer' },
+                })
+        })
+        -- Completing Settings end
+
+        --        local capabilities = NULL
+        --        local on_attach_callback = function(_, _)
+        --          vim.keymap.set()
+        --    end
+
+        -- Setup servers
+        lsp_config.rust_analyzer.setup {}
+        for _, value in pairs(servers_list) do
+            local server = lsp_config[value]
+            server.setup {
+                --                capabilities = capabilities,
+                --          on_attach = on_attach_callback,
+            }
+        end
+    end
+}
