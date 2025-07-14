04e2f4203865945621465e2c4a7a37146fea4aa6
Philippe Lopes <philippe.lopes@live.fr>
2025-07-14 12:27:12 +0200

    Fix aerial keymap

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -2,8 +2,8 @@ return {
     'stevearc/aerial.nvim',
     opts = {
         on_attach = function(bufnr)
-            vim.keymap.set(',', '{', '<cmd>AerialPrev<cr>', { buffer = bufnr })
-            vim.keymap.set(',', '}', '<cmd>AerialNext<cr>', { buffer = bufnr })
+            vim.keymap.set('n', '{', '<cmd>AerialPrev<cr>', { buffer = bufnr })
+            vim.keymap.set('n', '}', '<cmd>AerialNext<cr>', { buffer = bufnr })
         end,
         layout = {
             width = 0.2,
